bd1278c9ebfeb420d1131b79f2430d7ad6cb885b
Jacek Potrymajlo <>
2021-01-20 23:22:04 +0100

    Fixed handlers paramete and added mounting file

--- src/handlers.py
+++ src/handlers.py
@@ -1,4 +1,7 @@
 import pyfuse3
+import logging
+import os
+import errno
 
 logger = logging.getLogger(__name__)
 
@@ -6,84 +9,86 @@ class FuseHandlers(pyfuse3.Operations):
     def init(self):
         super(FuseHandlers, self).__init__()
     
-    async def access(inode, mode, ctx):
+    async def access(self, inode, mode, ctx):
         logger.info("access")
     
-    async def create(parent_inode, name, mode, flags, ctx):
+    async def create(self, parent_inode, name, mode, flags, ctx):
         logger.info("create")
 
-    async def flush(fh):
+    async def flush(self, fh):
         logger.info("flush")
 
-    async def forget(inode_list):
+    async def forget(self, inode_list):
         logger.info("forget")
     
-    async def fsync(fh, datasync):
+    async def fsync(self, fh, datasync):
         logger.info("fsync")
     
-    async def fsyncdir(fs, datasync):
+    async def fsyncdir(self, fs, datasync):
         logger.info("fsyncdir")
     
-    async def getattr(inode, ctx):
+    async def getattr(self, inode, ctx):
         logger.info("getattr")
+        attributes = pyfuse3.EntryAttributes()
+        return attributes
 
-    async def getxattr(inode, name, ctx):
+    async def getxattr(self, inode, name, ctx):
         logger.info("getxattr")
 
-    async def listxattr(inode, ctx):
+    async def listxattr(self, inode, ctx):
         logger.info("listxattr")
     
-    async def lookup(parent_inode, name, ctx):
+    async def lookup(self, parent_inode, name, ctx):
         logger.info("lookup")
     
-    async def mkdir(parent_inode, name, mode, ctx):
+    async def mkdir(self, parent_inode, name, mode, ctx):
         logger.info("mkdir")
     
-    async def mknod(parent_inode, name, mode, ctx):
+    async def mknod(self, parent_inode, name, mode, ctx):
         logger.info("mknod")
     
-    async def open(inode, flags, ctx):
+    async def open(self, inode, flags, ctx):
         logger.info("open")
         
-    async def opendir(inode, ctx):
+    async def opendir(self, inode, ctx):
         logger.info("opendir")
     
-    async def read(fh, off, size):
+    async def read(self, fh, off, size):
         logger.info("read")
     
-    async def readdir(fs, start_id, token):
+    async def readdir(self, fs, start_id, token):
         logger.info("readdir")
 
-    async def readlink(inode, ctx):
+    async def readlink(self, inode, ctx):
         logger.info("readlink")
     
-    async def release(fh):
+    async def release(self, fh):
         logger.info("releasedir")
     
-    async def removexattr(inode, name, ctx):
+    async def removexattr(self, inode, name, ctx):
         logger.info("removexattr")
     
-    async def rename(parent_inode_old, name_old, parent_inode_new, name_new, flags, ctx):
+    async def rename(self, parent_inode_old, name_old, parent_inode_new, name_new, flags, ctx):
         logger.info("rename")
     
-    async def rmdir(parent_inode, name, ctx):
+    async def rmdir(self, parent_inode, name, ctx):
         logger.info("rmdir")
     
-    async def setattr(indoe, attr, fields, fh, ctx):
+    async def setattr(self, inode, attr, fields, fh, ctx):
         logger.info("setattr")
     
-    async def setxattr(inode, name, value, ctx):
+    async def setxattr(self, inode, name, value, ctx):
         logger.info("setxattr")
     
-    async def statfs(ctx):
+    async def statfs(self, ctx):
         logger.info("statfs")
 
-    async def symlink(parent_inode, name, target, ctx)
+    async def symlink(self, parent_inode, name, target, ctx):
         logger.info("symlink")
     
-    async def unlink(parent_inode, name, ctx)
+    async def unlink(self, parent_inode, name, ctx):
         logger.info("unlink")
     
-    async def write(fs, off, buf):
+    async def write(self, fs, off, buf):
         logger.info("write")
 
--- src/main.py
+++ src/main.py
@@ -1,9 +1,22 @@
 import logging
 import handlers
+import argparse
+import pyfuse3
+import pyfuse3_asyncio
+import asyncio
 
 logger = logging.getLogger(__name__)
 
+def parse_cmdline():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("mountpoint", type=str, help="mounting point for filesystems")
+    return parser.parse_args()
+
 def main():
+    pyfuse3_asyncio.enable()
+
+    options = parse_cmdline()
+
     logger.setLevel(logging.DEBUG)
     sh = logging.StreamHandler()
     formatter = logging.Formatter('%(asctime)s [%(name)s %(levelname)s] %(message)s')
@@ -11,6 +24,26 @@ def main():
     logger.addHandler(sh)
 
     logger.info("start CloudDriveFs")
+
+    fuseHandlers = handlers.FuseHandlers()
+
+    fuseOptions = set(pyfuse3.default_options)
+    fuseOptions.add("fsname=DropboxFs")
+
+    pyfuse3.init(fuseHandlers, "/media/jacek/cloudfs", fuseOptions)
+
+    loop = asyncio.get_event_loop()
+
+    try:
+        loop.run_until_complete(pyfuse3.main())
+    except:
+        pyfuse3.close(unmount=False)
+        raise
+    finally:
+        loop.close()
+    
+    pyfuse3.close()
+    
 
 
 if __name__ == '__main__':
